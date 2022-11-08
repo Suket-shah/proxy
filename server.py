359ee6417eb3e1fe25a09d1a3fe9930b8d2e1091
Suket-shah <54611415+Suket-shah@users.noreply.github.com>
2022-11-08 11:13:49 +0800

    adding flag support

--- server.py
+++ server.py
@@ -1,22 +1,25 @@
 import socket
 import threading
+import sys
 
 # this probably needs to change to be the http port #
 
 MAX_REQUEST_LEN=1080
 CONECTION_TIMEOUT=1000
 SERVER = socket.gethostbyname(socket.gethostname())
-PORT=5050
 FORMAT='utf-8'
 
 class Server:
-  def __init__(self, hostname, bind_port) -> None:
+  def __init__(self, hostname, bind_port, img_flg, attack_flg) -> None:
+    self.img_flg = img_flg
+    self.attack_flg = attack_flg
     self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     self.serverSocket.bind((hostname, bind_port))
     self.serverSocket.listen()
     self.start_socket()
   
+  # TODO remove unecessary checkings from gfg
   def handle_client(self, conn, addr):
     print(f"new thread, {conn} and {addr}")
     request = conn.recv(MAX_REQUEST_LEN).decode(FORMAT)
@@ -50,12 +53,15 @@ class Server:
         webserver = temp[:port_pos]
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     s.settimeout(CONECTION_TIMEOUT)
+    print(f"webserver is {webserver}")
+    print(f"temp is {temp}")
     s.connect((webserver, port))
     s.sendall(request.encode(encoding=FORMAT))
     while 1:
     # receive data from web server
       data = s.recv(MAX_REQUEST_LEN)
-
+      # manipulate images if needed 
+      print(f"data is {data}")
       if (len(data) > 0):
           conn.send(data) # send to browser/client
       else:
@@ -71,5 +77,14 @@ class Server:
       thread.start()
 
 if __name__ == '__main__':
-  serve = Server(SERVER, PORT)
+  arguments = sys.argv[1:]
+  port = 5050
+  image_flg = 0
+  attack_flg = 0
+  if len(arguments) > 1:
+    port = arguments[0]
+    image_flg = arguments[1]
+    attack_flg = arguments[2]
+
+  serve = Server(SERVER, port, image_flg, attack_flg)
   serve.start_socket()
