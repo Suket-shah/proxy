7a1c4661b3981c74ab133e9ba61cec9d339f8e72
Suket-shah <54611415+Suket-shah@users.noreply.github.com>
2022-11-11 10:32:53 +0800

    attack mode working

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import sys
+import time
 
 # can't cache across different test cases.
 # but, you can cache within the same test case for the image. 
@@ -11,7 +12,7 @@ SERVER = socket.gethostbyname(socket.gethostname())
 FORMAT='utf-8'
 URL_OFFSET = 3
 
-def start_socket(port):
+def start_socket(port, image_flag, attack_flag):
   try: 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((SERVER, port))
@@ -23,16 +24,39 @@ def start_socket(port):
   while True:
     try:
       conn, addr = sock.accept()
-      data = conn.recv(5000).decode(FORMAT)
-      thread = threading.Thread(target=handle_client, args=(conn, addr, data), daemon=True)
-      thread.start()
+      client_host, client_port = addr
+      if attack_flag:
+        # conn, (client_host, client_port) = sock.accept()
+        print('Got connection from', client_host, client_port)
+        #c.send('Server Online\n') # This is invalid HTTP header
+        conn.recv(1000) # should receive request from client. (GET ....)
+        conn.send(b'HTTP/1.0 200 OK\n')
+        conn.send(b'Content-Type: text/html\n')
+        conn.send(b'\n') # header and body should be separated by additional newline
+        conn.send(b"""
+            <html>
+            <body>
+            <h1>You are being attacked</h1> 
+            </body>
+            </html>
+        """) # Use triple-quote string.
+        conn.close()
+      else:
+        data = conn.recv(4084).decode(FORMAT)
+        print('receiving data ', data)
+        thread = threading.Thread(target=handle_client, args=(conn, addr, data, image_flag, attack_flag), daemon=True)
+        thread.start()
     except KeyboardInterrupt:
       sock.close()
       print("Shutting down")
       sys.exit(1)
+    except UnicodeDecodeError:
+      sock.close()
+      print("some non utf-8 encoding was used")
+      sys.exit(1)
 
-def handle_client(conn, addr, data):
-  if data.find("GET") == -1 or data.find("favicon") != -1:
+def handle_client(conn, addr, data, image_flag, attack_flag):
+  if data.find("GET") == -1:
     return
   print(f"starting thread with addr {addr}")
   url = data.split('\n')[0].split(' ')[1]
@@ -57,6 +81,7 @@ def handle_client(conn, addr, data):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((server, port))
     sock.send(data.encode(encoding=FORMAT))
+    # print('sending data ', data)
     fail_ct = 0
     while 1:
       reply = sock.recv(5000)
@@ -65,6 +90,7 @@ def handle_client(conn, addr, data):
         conn.sendall(reply)
         fail_ct = 0
       elif len(reply) <= 0 and fail_ct < 2:
+        time.sleep(.5)
         fail_ct += 1
       else:
         print("we are breaking")
@@ -90,5 +116,5 @@ if __name__ == '__main__':
   if len(arguments) > 2:
     attack_flg = arguments[2]
   print(f"Listening to port {port}")
-  start_socket(port)
+  start_socket(port, image_flg, attack_flg)
 
